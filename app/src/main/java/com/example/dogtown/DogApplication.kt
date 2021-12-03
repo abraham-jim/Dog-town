c7735a1e5fc25751596e8c512ff235237d1ac957
abraham-jim <abrahamjima@gmail.com>
2021-12-02 22:46:28 -0800

    Modified DogApplication, DogImageApiService and deleted RandomDogApiResponde

--- app/src/main/java/com/example/dogtown/DogApplication.kt
+++ app/src/main/java/com/example/dogtown/DogApplication.kt
@@ -2,9 +2,11 @@ package com.example.dogtown
 
 import android.app.Application
 import com.example.dogtown.database.AppDatabase
+import kotlinx.coroutines.InternalCoroutinesApi
 
 
 class DogApplication : Application() {
 
+    @OptIn(InternalCoroutinesApi::class)
     val database: AppDatabase by lazy { AppDatabase.getDatabase(this) }
 }
